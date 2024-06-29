d1a1bd2dca1ce86ce79ce59a115a1b70d73e20ec
Ahmeddik <ahmeddik22@gmail.com>
2024-06-29 22:35:47 +0300

    front end

---
+++ Students_Records/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Students_Records
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            label7 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            textBox6 = new TextBox();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            FirstName = new DataGridViewTextBoxColumn();
+            LastName = new DataGridViewTextBoxColumn();
+            Email = new DataGridViewTextBoxColumn();
+            Grade = new DataGridViewTextBoxColumn();
+            Section = new DataGridViewTextBoxColumn();
+            DateOfBirth = new DataGridViewTextBoxColumn();
+            button3 = new Button();
+            button4 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-2, -4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(634, 715);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaption;
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(textBox6);
+            panel2.Location = new Point(638, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(621, 706);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(61, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 25);
+            label1.TabIndex = 0;
+            label1.Text = "First Name";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(191, 148);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(371, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(191, 233);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(371, 31);
+            textBox2.TabIndex = 3;
+            //textBox2.TextChanged += this.textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(61, 233);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Last Name";
+            //label2.Click += this.label2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(191, 316);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(371, 31);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(61, 316);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Email";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(191, 403);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(371, 31);
+            textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(61, 403);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Grade";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(191, 476);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(371, 31);
+            textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(61, 476);
+            label5.Name = "label5";
+            label5.Size = new Size(70, 25);
+            label5.TabIndex = 8;
+            label5.Text = "Section";
+            //label5.Click += this.label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(61, 560);
+            label6.Name = "label6";
+            label6.Size = new Size(112, 25);
+            label6.TabIndex = 10;
+            label6.Text = "Date of Birth";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Cyan;
+            button1.Location = new Point(450, 638);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Regester";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Showcard Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = Color.Blue;
+            label7.Location = new Point(121, 52);
+            label7.Name = "label7";
+            label7.Size = new Size(399, 40);
+            label7.TabIndex = 12;
+            label7.Text = "Student Regestration";
+            label7.Click += label7_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(191, 554);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(371, 31);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(32, 58);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(371, 31);
+            textBox6.TabIndex = 13;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(442, 58);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 13;
+            button2.Text = "Search";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { FirstName, LastName, Email, Grade, Section, DateOfBirth });
+            dataGridView1.Location = new Point(3, 155);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(615, 477);
+            dataGridView1.TabIndex = 14;
+            // 
+            // FirstName
+            // 
+            FirstName.HeaderText = "First Name";
+            FirstName.MinimumWidth = 8;
+            FirstName.Name = "FirstName";
+            FirstName.Width = 150;
+            // 
+            // LastName
+            // 
+            LastName.HeaderText = "Last Name";
+            LastName.MinimumWidth = 8;
+            LastName.Name = "LastName";
+            LastName.Width = 150;
+            // 
+            // Email
+            // 
+            Email.HeaderText = "Email";
+            Email.MinimumWidth = 8;
+            Email.Name = "Email";
+            Email.Width = 150;
+            // 
+            // Grade
+            // 
+            Grade.HeaderText = "Grade";
+            Grade.MinimumWidth = 8;
+            Grade.Name = "Grade";
+            Grade.Width = 150;
+            // 
+            // Section
+            // 
+            Section.HeaderText = "Section";
+            Section.MinimumWidth = 8;
+            Section.Name = "Section";
+            Section.Width = 150;
+            // 
+            // DateOfBirth
+            // 
+            DateOfBirth.HeaderText = "Date Of Birth";
+            DateOfBirth.MinimumWidth = 8;
+            DateOfBirth.Name = "DateOfBirth";
+            DateOfBirth.Width = 150;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.OrangeRed;
+            button3.Location = new Point(487, 649);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 13;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.SpringGreen;
+            button4.Location = new Point(350, 649);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 13;
+            button4.Text = "Edit";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1260, 705);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Student Records";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label7;
+        private Button button1;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
+        private Button button2;
+        private TextBox textBox6;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn FirstName;
+        private DataGridViewTextBoxColumn LastName;
+        private DataGridViewTextBoxColumn Email;
+        private DataGridViewTextBoxColumn Grade;
+        private DataGridViewTextBoxColumn Section;
+        private DataGridViewTextBoxColumn DateOfBirth;
+        private Button button4;
+        private Button button3;
+    }
+}
---
+++ Students_Records/Form1.cs
@@ -0,0 +1,30 @@
+namespace Students_Records
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
